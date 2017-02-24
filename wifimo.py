17f7aa7bcea62b1f0ef5f92569c538127dd69e50
mdd <info@mddcore.com>
2017-02-24 17:17:32 +0200

    Second linux command added

--- wifimo.py
+++ wifimo.py
@@ -1,12 +1,16 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
 import os
 import subprocess
 
 # Linux command to execute to get wlan0 bytes for bandwidth
 
+### Incoming traffic
 proc=subprocess.Popen("ifconfig wlan0 |grep bytes | cut -d\":\" -f2 |cut -d\" \" -f1", shell=True, stdout=subprocess.PIPE, )
-output=proc.communicate()[0]
+incoming=proc.communicate()[0]
 
-print output
+### Outgoing traffic
+
+proc=subprocess.Popen("ifconfig wlan0 |grep bytes | cut -d\":\" -f3 |cut -d\" \" -f1", shell=True, stdout=subprocess.PIPE, )
+outgoing=proc.communicate()[0]
+
+
+print incoming, outgoing
